eba5667dbd7ed1a8ab3f9088b403519670763c47
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-25 17:02:30 -0300

    Criado POST

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const url = 'http://localhost:5500/api'
 
+const newUser = {
+    name: "Spyro",
+    avatar: "https://picsum.photos/200",
+    city: "Rio de Janeiro"
+}
+
+
+// GET
 const getUser = () => {
     axios.get(url)
         .then(resposnse => {
@@ -10,3 +18,14 @@ const getUser = () => {
 }
 
 getUser()
+
+// POST
+const addNewUser = () => {
+    axios.post(url, newUser)
+        .then(response => {
+            alert(JSON.stringify(response.data))
+        })
+        .catch(e => console.log(e))
+}
+
+addNewUser()
