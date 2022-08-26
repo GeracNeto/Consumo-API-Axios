2279b2c3cd05d45f505420ce766d4ef6f88c3539
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-26 15:17:39 -0300

    Criado consumo de API do github com axios

--- React/data-fetching/src/App.tsx
+++ React/data-fetching/src/App.tsx
@@ -1,8 +1,37 @@
+import axios from "axios"
+
+import { useEffect, useState } from "react"
+
+type RepositoryProps = {
+  full_name: string,
+  description: string
+}
+
+const url = 'http://api.github.com/users/geracneto/repos'
 
 function App() {
 
+  const [repositories, setRepositories] = useState<RepositoryProps[]>([])
+
+  useEffect(() => {
+    axios.get(url)
+      .then(response => {
+        setRepositories(response.data)
+      })
+      .catch(e => console.log(e))
+  }, [])
+
   return (
-    <h1>Hello World</h1>
+    <div className="App">
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
